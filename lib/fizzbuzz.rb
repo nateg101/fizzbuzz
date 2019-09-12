010161f9a64ce8beac1a76307c1480b2c12a39b8
Heli Sivunen <heli@mooncat.tv>
2019-09-12 14:05:18 +0100

    Passes 4th test.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,8 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  
+  if number % 5 == 0 && number % 3 == 0
+    return "fizzbuzz"
+  elsif number % 3 == 0
     return "fizz"
   elsif number % 5 == 0
     return "buzz"
