1cda8ece5b492dbdf1a97df93e86307ed896a1f4
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-09-12 13:40:03 +0100

    Second rspec test written

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,17 @@
 def fizzbuzz(number)
   'fizz'
 end
+
+=begin
+1.upto(20) do |i|
+  if i % 5 == 0 and i % 3 == 0
+    puts "FizzBuzz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  else
+    puts i
+  end
+end
+=end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,12 @@
-require_relative 'fizzbuzz'
+require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns 4 when passed 4' do
+    expect(fizzbuzz(4)). to eq 4
   end
 end
